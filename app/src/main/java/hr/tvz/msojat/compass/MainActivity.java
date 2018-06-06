f66bf628cdb83c08f2fc71e3ffefae75bc984b90
Marko Sojat <marko.sojat@verso.hr>
2018-06-06 10:01:15 +0200

    App made with Camera API and attempted to change to Camera2 API

--- app/src/main/java/hr/tvz/msojat/compass/MainActivity.java
+++ app/src/main/java/hr/tvz/msojat/compass/MainActivity.java
@@ -2,6 +2,9 @@ package hr.tvz.msojat.compass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import hr.tvz.msojat.compass.view.ArSurfaceView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.ar_view);
+
+        ArSurfaceView arSurfaceView = new ArSurfaceView(this, this);
+        frameLayout.addView(arSurfaceView);
+
+        OverlayView overlayView = new OverlayView(getApplicationContext());
+        frameLayout.addView(overlayView);
     }
 }
